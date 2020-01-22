abf1b9bce6028741da7b4c1da707fe278a14df32
chrystian duarte <cduarte@localhost.localdomain>
2020-01-22 16:03:44 -0300

    modificacion de GUID

--- blackjack-frontend/public/blackjack.js
+++ blackjack-frontend/public/blackjack.js
@@ -56,7 +56,7 @@ resetBoard, showBoard, showAlert, getWinner, jQuery, wager */
 			ele   = '',
 			score = '';
 
-		var guid = 'santiago-8bee';
+		var guid = 'santiago-8980';
 		var api_payment = 'http://blackjack-payment-blackjack.apps.'+guid+'.open.redhat.com/blackjack';
 		var api_users = 'http://blackjack-users-blackjack.apps.'+guid+'.open.redhat.com/blackjack';
 
--- blackjack-frontend/public/dashboard.js
+++ blackjack-frontend/public/dashboard.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var refreshRate = '5000';
-    var guid = 'santiago-8bee';
+    var guid = 'santiago-8980';
     var api_ranking = 'http://blackjack-ranking-blackjack.apps.'+guid+'.open.redhat.com/blackjack/user/ranking';
 
     $.getJSON(api_ranking, function (response) {
